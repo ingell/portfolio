b7edb162a0f56afb20bb0469f6505667f357c96d
Elena <ingelsnipes@gmail.com>
2025-07-04 14:18:31 +0200

    Added image, scroll and socials

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,44 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
-
+const navMennu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
-
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMennu.classList.add("show-menu");
+  });
+}
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
-
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMennu.classList.remove("show-menu");
+  });
+}
 /*==================== REMOVE MENU MOBILE ====================*/
-
+const navLink = document.querySelectorAll("nav_link");
+function linkAction() {
+  const navMenu = document - getElementById("nav-menu");
+  // When we click on each nav link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
 
-
 /*==================== QUALIFICATION TABS ====================*/
-
 
 /*==================== SERVICES MODAL ====================*/
 
-
 /*==================== PORTFOLIO SWIPER  ====================*/
-
 
 /*==================== TESTIMONIAL ====================*/
 
-
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
-
-/*==================== SHOW SCROLL UP ====================*/ 
-
-
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
