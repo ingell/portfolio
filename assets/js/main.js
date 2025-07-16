52931cb212cdd79ffc0e6d6063192c239b326d6d
Elena <ingelsnipes@gmail.com>
2025-07-16 18:21:43 +0200

    Feat: Added projects and testimonial view

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,19 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-const navMennu = document.getElementById("nav-menu"),
+const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
   navClose = document.getElementById("nav-close");
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if (navToggle) {
   navToggle.addEventListener("click", () => {
-    navMennu.classList.add("show-menu");
+    navMenu.classList.add("show-menu");
   });
 }
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 if (navClose) {
   navClose.addEventListener("click", () => {
-    navMennu.classList.remove("show-menu");
+    navMenu.classList.remove("show-menu");
   });
 }
 /*==================== REMOVE MENU MOBILE ====================*/
@@ -62,11 +62,65 @@ tabs.forEach((tab) => {
     tab.classList.add("qualification__active");
   });
 });
+
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalBtns = document.querySelectorAll(".services__button"),
+  modalCloses = document.querySelectorAll(".services__modal-close");
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
+});
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
+let swiperPortfolio = new Swiper(".portfolio__container", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
 /*==================== TESTIMONIAL ====================*/
+
+let swiperTestimonial = new Swiper(".testimonial__container", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 48,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    568: {
+      slidesPerView: 2,
+    },
+  },
+});
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
